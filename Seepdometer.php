e8f360aacb022f7137e4664962f61f751ea5653d
Maxence Hattabi <105151136+MaxenceCodeFR@users.noreply.github.com>
2022-11-10 17:17:54 +0100

    Update Seepdometer.php

--- Seepdometer.php
+++ Seepdometer.php
@@ -5,13 +5,13 @@ class Speedometer
 
     public static function convertKmToMiles($km)
     {
-        $miles = round($km * self::KM_TO_MILES, 2, PHP_ROUND_HALF_DOWN);
+        $miles = round($km * self::KM_TO_MILES);
         return $km . " km-miles: " . $miles;
     }
 
     public static function convertMilesToKm($miles)
     {
-        $km = round($miles * self::MILE_TO_KM, 2, PHP_ROUND_HALF_DOWN);
+        $km = round($miles * self::MILE_TO_KM);
         return $miles . " mile-km: " . $km;
     }
 }
